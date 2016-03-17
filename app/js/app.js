8ee68b3e91375c9265a9eab2d6855ba70fcaa791
wangmingdong <wangmingdong@navinfo.com>
2016-03-17 10:51:27 +0800

    merge

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,9 @@
 
-var app = angular.module('weApp',[]);
-console.log(app)
-app.controller('MainCtrl',['$scope',function($scope){
-	console.log('main')
-	$scope.navContainer = 'pages/navbar.html';
-}]);
-
-/*var app = angular.module('weApp',['oc.lazyLoad','ui.router']);
+var app = angular.module('weApp',['oc.lazyLoad','ui.router']);
 app.controller('MainCtrl',['$scope','$ocLazyLoad',function($scope,$ocLazyLoad){
 	console.log('main')
-	$scope.navContainer = 'pages/navbar.html';
+	$ocLazyLoad.load('js/controllers/navCtrl').then(function () {
+		alert()
+		$scope.navContainer = 'pages/navbar.html';
+	});
 }]);
-*/
