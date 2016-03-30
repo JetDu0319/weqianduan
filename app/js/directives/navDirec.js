971c007574892892cd127dd849d996840aa51e9d
wangmingdong <wangmingdong@navinfo.com>
2016-03-30 09:35:55 +0800

    解决指令异步加载问题

--- app/js/directives/navDirec.js
+++ app/js/directives/navDirec.js
@@ -1,12 +1,11 @@
 define(['app'],function(app){
     app.register
-    .directive('slideBtn', function($scope){
+    .directive('slideBtn', function(){
         return {
 			restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
 			template: '',
 			replace: false,
 			link: function($scope, elem, attrs, controller) {
-				alert()
 				elem.bind('click',function(){
 					console.log(this)
 				});
