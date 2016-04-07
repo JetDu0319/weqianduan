a01a4b320943f06bf38e9f342befe153e301896e
wangmingdong <wangmingdong@navinfo.com>
2016-04-07 20:03:27 +0800

    加入uglify包

--- app/src/gulpfile.js
+++ app/src/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     notify = require('gulp-notify'),
-    uglify = require('gulp-uglify'),
+    uglify = require('gulp-uglify'),	//压缩js文件
     plumber = require('gulp-plumber');
 /*处理less文件*/ 
 gulp.task('appLess', function () {
